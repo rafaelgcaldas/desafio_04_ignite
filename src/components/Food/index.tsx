db3bc035a555af554e609523e2078fc6097eadd8
Rafael <rafael.gcaldas01@gmail.com>
2021-09-14 22:11:34 -0300

    Migrating ModalAddFood Component

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -25,7 +25,7 @@ export const Food = ({ food, handleDelete, handleEditFood }: FoodProps) => {
   useEffect(() => {
     const { available } = food;
     setIsAvailable(available);
-  }, [])
+  }, [food])
 
   const toggleAvailable = async () => {
 
